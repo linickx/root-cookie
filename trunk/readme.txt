21eae79c760ae27d10050ac6adbcded24e7d58b5
sc0ttkclark <sc0ttkclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-26 17:19:16 +0000

    1.5 for approval
    git-svn-id: https://plugins.svn.wordpress.org/root-cookie@97998 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/readme.txt
+++ trunk/readme.txt
@@ -1,12 +1,12 @@
 === Plugin Name ===
-Contributors: Nick  Bettison - LINICKX
+Contributors: Nick  Bettison - LINICKX, sc0ttkclark
 Donate link: http://www.linickx.com/index.php?content=donate
 Tags: login, logout, authentication, cookie
 Requires at least: 2.6
 Tested up to: 2.7.1
 Stable tag: 1.4
 
-root-cookie changes the path of your cookie to the full domain; i.e. from mydomain.com/wordpress to mydomain.com
+Changes the cookie default path to / (i.e. the whole domain.com not just domain.com/blog) with an option to go across subdomains
 
 == Description ==
 
@@ -14,7 +14,15 @@ If you want to integrate the wordpress authentication magic into another script 
 
 By default the wordpress cookie exactly matches the URL of your installation, this plugin removes any subfolders from the cookie so that your whole domain has access to it.
 
-For Example if you have wordpress installed in http://mydomain.com/wordpress any php stored in http://mydomain.com/mymagiccode cannot see the cookie due to browser security. This plugin changes the path to http://mydomain.com so that any php code on your site can access the cookie, so in our above example http://mydomain.com/mymagiccode/checkauthentication.php can now check the cookie to see if you have logged into wordpress.
+For Example if you have wordpress installed in http://www.mydomain.com/wordpress/ any php stored in http://www.mydomain.com/mymagiccode/ cannot see the cookie due to browser security. This plugin changes the path to http://www.mydomain.com/ so that any php code on your site can access the cookie, so in our above example http://www.mydomain.com/mymagiccode/checkauthentication.php can now check the cookie to see if you have logged into wordpress.
+
+NEW: Now you can allow cookies to be used across subdomains! Here are some examples that you can now access cookies from:
+
+* http://www.mydomain.com/
+* http://test.mydomain.com/
+* http://www.mydomain.com/blog/
+* http://test.mydomain.com/new/
+* http://forum.mydomain.com/getwordpresslogin.php
 
 == Installation ==
 
@@ -34,6 +42,7 @@ e.g.
 * 1.2 : Added logout function, as default on didn't work, thanks Aja! ( http://www.ajalapus.com/  )
 * 1.3 : Updated to support new WP2.6 Cookies, credz to Scott Kingsley Clark  (http://www.vizioninteractive.com) for kicking my ass into getting this done!
 * 1.4 : WordPress 2.7 Compatability Update, credz to Edward Laverick (http://www.rndout.com/) for Raising.
+* 1.5 : Added options panel to allow for cookies to be used across subdomains.
 
 == Support ==
 
--- trunk/root-cookie.php
+++ trunk/root-cookie.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: root Cookie Path
 Plugin URI: http://www.linickx.com/archives/831/root-cookie-path-14-an-update-for-wordpress-27
-Description: Changes the cookie default path to / (i.e. the whole domain.com not just domain.com/blog)
-Author: Nick [LINICKX] Bettison
-Version: 1.4
+Description: Changes the cookie default path to / (i.e. the whole domain.com not just domain.com/blog) with an option to go across subdomains
+Author: Nick [LINICKX] Bettison and Vizion Interactive, Inc
+Version: 1.5
 Author URI: http://www.linickx.com
 License: Free to use non-commercially.
 Warranties: None.
@@ -58,18 +58,32 @@ function wp_set_auth_cookie($user_id, $remember = false, $secure = '') {
 
 	do_action('set_auth_cookie', $auth_cookie, $expire, $expiration, $user_id, $scheme);
 	do_action('set_logged_in_cookie', $logged_in_cookie, $expire, $expiration, $user_id, 'logged_in');
+	
+	$subdomain = get_option('rootcookie_subdomain');
+	if($subdomain==1)
+		{
+			$info = get_bloginfo('url');
+			$info = parse_url($info);
+			$info = $info['host'];
+			$exp = explode('.',$info);
+			if(count($exp)==3){$domain = '.'.$exp[1].'.'.$exp[2];}
+			elseif(count($exp)==2){$domain = '.'.$info;}
+			elseif(3<count($exp)){$exp = array_reverse($exp); $domain = '.'.$exp[1].'.'.$exp[0];}
+			else{$domain = COOKIE_DOMAIN;}
+		}
+	else{$domain = COOKIE_DOMAIN;}
 
 	// Set httponly if the php version is >= 5.2.0
 	if ( version_compare(phpversion(), '5.2.0', 'ge') ) {
-		setcookie($auth_cookie_name, $auth_cookie, $expire, ROOT_COOKIE, COOKIE_DOMAIN, $secure);
+		setcookie($auth_cookie_name, $auth_cookie, $expire, ROOT_COOKIE, $domain, $secure);
 		/** Duplicate of above - Created by Find & Replace
-		setcookie($auth_cookie_name, $auth_cookie, $expire, ROOT_COOKIE, COOKIE_DOMAIN, $secure);
+		setcookie($auth_cookie_name, $auth_cookie, $expire, ROOT_COOKIE, $domain, $secure);
 		*/
-		setcookie(LOGGED_IN_COOKIE, $logged_in_cookie, $expire, ROOT_COOKIE, COOKIE_DOMAIN);
+		setcookie(LOGGED_IN_COOKIE, $logged_in_cookie, $expire, ROOT_COOKIE, $domain);
 		if ( COOKIEPATH != SITECOOKIEPATH )
-			setcookie(LOGGED_IN_COOKIE, $logged_in_cookie, $expire, ROOT_COOKIE, COOKIE_DOMAIN, false, true);
+			setcookie(LOGGED_IN_COOKIE, $logged_in_cookie, $expire, ROOT_COOKIE, $domain, false, true);
 	} else {
-		$cookie_domain = COOKIE_DOMAIN;
+		$cookie_domain = $domain;
 		if ( !empty($cookie_domain) )
 			$cookie_domain .= '; HttpOnly';
 		setcookie($auth_cookie_name, $auth_cookie, $expire, ROOT_COOKIE, $cookie_domain, $secure);
@@ -78,7 +92,7 @@ function wp_set_auth_cookie($user_id, $remember = false, $secure = '') {
 		*/
 		setcookie(LOGGED_IN_COOKIE, $logged_in_cookie, $expire, ROOT_COOKIE, $cookie_domain);
 		if ( ROOT_COOKIE != SITECOOKIEPATH )
-			setcookie(LOGGED_IN_COOKIE, $logged_in_cookie, $expire, ROOT_COOKIE, COOKIE_DOMAIN);
+			setcookie(LOGGED_IN_COOKIE, $logged_in_cookie, $expire, ROOT_COOKIE, $domain);
 	}		
 }
 
@@ -92,26 +106,95 @@ if ( !function_exists('wp_clear_auth_cookie') ) :
  */
 function wp_clear_auth_cookie() {
 	do_action('clear_auth_cookie');
+	
+	$subdomain = get_option('rootcookie_subdomain');
+	if($subdomain==1)
+		{
+			$info = get_bloginfo('url');
+			$info = parse_url($info);
+			$info = $info['host'];
+			$exp = explode('.',$info);
+			if(count($exp)==3){$domain = '.'.$exp[1].'.'.$exp[2];}
+			elseif(count($exp)==2){$domain = '.'.$info;}
+			elseif(3<count($exp)){$exp = array_reverse($exp); $domain = '.'.$exp[1].'.'.$exp[0];}
+			else{$domain = COOKIE_DOMAIN;}
+		}
+	else{$domain = COOKIE_DOMAIN;}
 
-	setcookie(AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(LOGGED_IN_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(LOGGED_IN_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, COOKIE_DOMAIN);
+	setcookie(AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(LOGGED_IN_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(LOGGED_IN_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, $domain);
 
 	// Old cookies
-	setcookie(AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(AUTH_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, COOKIE_DOMAIN);
-	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, COOKIE_DOMAIN);
+	setcookie(AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(AUTH_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, $domain);
+	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(SECURE_AUTH_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, $domain);
 
 	// Even older cookies
-	setcookie(USER_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(PASS_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, COOKIE_DOMAIN);
-	setcookie(USER_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, COOKIE_DOMAIN);
-	setcookie(PASS_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, COOKIE_DOMAIN);
+	setcookie(USER_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(PASS_COOKIE, ' ', time() - 31536000, ROOT_COOKIE, $domain);
+	setcookie(USER_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, $domain);
+	setcookie(PASS_COOKIE, ' ', time() - 31536000, SITECOOKIEPATH, $domain);
 }
 endif;
 
+function rootcookie_activate ()
+	{
+		$opt_val = get_option('rootcookie_subdomain');
+		if($opt_val!=1)
+			{
+				delete_option('rootcookie_subdomain');
+				add_option('rootcookie_subdomain',0);
+			}
+	}
+function rootcookie_menu ()
+	{
+		add_options_page('root Cookie Path Options', 'root Cookie Path', 8, __FILE__, 'rootcookie_options');
+	}
+function rootcookie_options ()
+	{
+		// variables for the field and option names
+		$opt_name = 'rootcookie_subdomain';
+		$hidden_field_name = 'rootcookie_submit_hidden';
+	
+		// Read in existing option value from database
+		$opt_val = get_option( $opt_name );
+		$checked=false;
+		if($opt_val==1){$checked=true;}
+	
+		// See if the user has posted us some information
+		// If they did, this hidden field will be set to 'Y'
+		if( $_POST[ $hidden_field_name ] == 'Y' )
+			{
+				$opt_val = 0;
+				if(isset($_POST[ $opt_name ]))
+					{
+						$opt_val = 1;
+					}
+				update_option( $opt_name, $opt_val );
+				echo '<div class="updated"><p><strong>'._('Options saved.', 'cookiepath_trans_domain' ).'</strong></p></div>';
+			}
+?>
+<div class="wrap">
+<?php echo "<h2>" . __( 'root Cookie Path Options', 'rootcookie_trans_domain' ) . "</h2>"; ?>
+<form name="plugin_options" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y" />
+<p><?php _e("Allow Cookies to go across All Subdomains:", 'rootcookie_trans_domain' ); ?> 
+<input type="checkbox" name="<?php echo $opt_name; ?>" value="<?php echo $opt_val; ?>"<?php if($checked){echo " CHECKED";} ?> />
+</p><hr />
+<p class="submit">
+<input type="submit" name="Submit" value="<?php _e('Update Options', 'rootcookie_trans_domain' ) ?>" />
+</p>
+</form>
+</div>
+<?php
+	}
+
+// Run all actions and hooks at the end to keep it tidy
+add_action('admin_menu', 'rootcookie_menu');
+register_activation_hook( __FILE__,'rootcookie_activate');
 ?>
