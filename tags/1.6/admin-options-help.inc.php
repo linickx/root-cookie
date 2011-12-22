f2c28a3f4b078220012fc15143accfd41bdd6dae
linickx <linickx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-22 22:00:59 +0000

    1.6 is good to go!
    git-svn-id: https://plugins.svn.wordpress.org/root-cookie@479559 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.6/admin-options-help.inc.php
@@ -0,0 +1,12 @@
+<p><strong>Root Cookie</strong> is a simple plugin that changes the paths associated with your WordPress cookies.</p>
+<p>
+By default the WordPress cookies are matched to your blog url such as www.example.com/blog which means that the cookies cannot accessed by scripts within www.example.com/gallery or even www.example.com.
+</p>
+<p>
+Root cookie will work without any configuration, simply activate the plugin, clear your cookie cache and re-log in.
+</p>
+<p>
+<strong>Sub Domain Support</strong> <br />
+Optionally you can enable subdomain support, this will allow WordPress cookies to be available to test.example.com or something.example.com. Simply tick the tickbox "<em>Allow Cookies to go across All Subdomains</em>" and you are done. <br />
+<em>Sometimes</em> the tickbox gets the subdomain wrong, in this case, un-tick the checkbox and fill in the domain name and click "<em>Update Options</em>"
+</p>
